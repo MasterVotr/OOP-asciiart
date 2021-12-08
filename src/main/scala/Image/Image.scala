d5137e6af2960a91fb7b36c2dba35b04733b08c0
Jakub Votrubec <votruja6@fit.cvut.cz>
2021-12-08 15:52:45 +0100

    Started implementing Flip, Rotate and Scale

--- src/main/scala/Image/Image.scala
+++ src/main/scala/Image/Image.scala
@@ -1,8 +1,13 @@
 package Image
 
+import Filter.ImageFilter.TransformFilter.{FlipAxisEnum, Flipable, Rotable, RotationEnum, Scalable, ScalingEnum}
 import Image.PixelGrid.Pixel.Pixel
 import Image.PixelGrid.PixelGrid
 
-abstract class Image[P <: Pixel] (pixelGrid: PixelGrid[P]) {
+abstract class Image[P <: Pixel] (pixelGrid: PixelGrid[P]) extends Flipable with Rotable with Scalable {
+    override def Flip(flipAxis: FlipAxisEnum): Unit = pixelGrid.Flip(flipAxis)
 
+    override def Rotate(rotation: RotationEnum): Unit = pixelGrid.Rotate(rotation)
+
+    override def Scale(scale: ScalingEnum): Unit = pixelGrid.Scale(scale)
 }
--- src/main/scala/Image/PixelGrid/PixelGrid.scala
+++ src/main/scala/Image/PixelGrid/PixelGrid.scala
@@ -1,8 +1,10 @@
 package Image.PixelGrid
 
+import Filter.ImageFilter.TransformFilter.{FlipAxisEnum, FlipX, FlipY, Flipable, Rot180, Rot270, Rot90, Rotable, RotationEnum, Scalable, ScaleDouble, ScaleFull, ScaleHalf, ScalingEnum}
+import Filter.PixelFilter.PixelFilter
 import Image.PixelGrid.Pixel.Pixel
 
-abstract class PixelGrid[P <: Pixel] {
+abstract class PixelGrid[P <: Pixel] extends Flipable with Rotable with Scalable {
     var width: Int = 0
     var height: Int = 0
     private var pixels: Array[Array[P]] = new Array[Array[P]](0)
@@ -25,5 +27,67 @@ abstract class PixelGrid[P <: Pixel] {
         this.width = _width
         this.height = _height
         this.pixels = _pixels
+    }
+
+    def ApplyFilter(filter: PixelFilter[P]): Unit = {
+        for(y <- 0 until height) {
+            for(x <- 0 until width) {
+                filter.applyFiler(pixels(y)(x))
+            }
+        }
+    }
+
+    def Flip(flipAxis: FlipAxisEnum): Unit = {
+        flipAxis match {
+            case FlipX => this.XFlip()
+            case FlipY => this.YFlip()
+        }
+    }
+
+    def XFlip(): Unit = {
+        // TODO
+    }
+
+    def YFlip(): Unit = {
+        // TODO
+    }
+
+    def Rotate(rotation: RotationEnum): Unit = {
+        rotation match {
+            case Rot90 => Rotate90deg()
+            case Rot180 => {
+                Rotate90deg()
+                Rotate90deg()
+            }
+            case Rot270 => {
+                Rotate90deg()
+                Rotate90deg()
+                Rotate90deg()
+            }
+        }
+    }
+
+    def Rotate90deg(): Unit = {
+        // TODO
+    }
+
+    def Scale(scale: ScalingEnum): Unit = {
+        scale match {
+            case ScaleHalf => this.HalfScale()
+            case ScaleDouble => this.DoubleScale()
+            case ScaleFull => Unit
+        }
+    }
+
+    def HalfScale(): Unit = {
+        // TODO
+    }
+
+    def DoubleScale(): Unit = {
+        // TODO
+    }
+
+    def getPixel(x: Int, y: Int): P = {
+        pixels(y)(x)
     }
 }
