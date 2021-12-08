d59d0083fac96f75faba0930b3b0eb2f9f01cd80
Jakub Votrubec <votruja6@fit.cvut.cz>
2021-12-08 15:59:58 +0100

    Added GreyscaleImage class

---
+++ src/main/scala/Image/GreyscaleImage.scala
@@ -0,0 +1,8 @@
+package Image
+
+import Image.PixelGrid.Pixel.GreyscalePixel
+import Image.PixelGrid.GreyscalePixelGrid
+
+class GreyscaleImage (pixelGrid: GreyscalePixelGrid) extends Image[GreyscalePixel] (pixelGrid) {
+
+}
