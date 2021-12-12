8e31d362cee83ab2acbe074811eed9d6649e2c7b
Jakub Votrubec <votruja6@fit.cvut.cz>
2021-12-12 23:54:47 +0100

    Fixed ImageImporters

--- src/main/scala/ASCIIArt/ImageImporter/FileImageImporter/JPGFileImageImporter.scala
+++ src/main/scala/ASCIIArt/ImageImporter/FileImageImporter/JPGFileImageImporter.scala
@@ -1,6 +1,7 @@
 package ASCIIArt.ImageImporter.FileImageImporter
+
 import ASCIIArt.Image.PixelGrid.Pixel.RGBPixel
-import ASCIIArt.Image.PixelGrid.RGBPixelGrid
+import ASCIIArt.Image.PixelGrid.PixelGrid
 import ASCIIArt.Image.RGBImage
 
 import java.io.File
@@ -10,15 +11,23 @@ class JPGFileImageImporter(path: String) extends FileImageImporter(path) {
   override def ImportImage(): RGBImage = {
     val img = ImageIO.read(new File(path))
     if (img == null)
-      throw new Exception("File format not supported!")
+      throw new Exception("File not loaded properly!")
     val width = img.getWidth()
     val height = img.getHeight()
-    val pixelGrid = new RGBPixelGrid()
+
+    // Check resolution
+    if (width < 1 || height< 1)
+      throw new Exception("Resolution cannot be <= 0!")
+
+    var pixelGrid = new PixelGrid[RGBPixel]()
     for (y <- 0 until height) {
-      val pixelLine = Vector[RGBPixel]()
+      var pixelLine = Vector[RGBPixel]()
       for (x <- 0 until width)
-        pixelLine.appended(new RGBPixel(img.getRGB(x, y)))
-      pixelGrid.appended(pixelLine.toArray)
+        pixelLine = pixelLine.appended(new RGBPixel(img.getRGB(x, y)))
+      // Check line length
+      if (pixelLine.length != width)
+        throw new Exception("Pixels, wrong width!")
+      pixelGrid = pixelGrid.appended(pixelLine)
     }
     RGBImage(pixelGrid)
   }
--- src/main/scala/ASCIIArt/ImageImporter/RandomImageImporter/RandomImageImporter.scala
+++ src/main/scala/ASCIIArt/ImageImporter/RandomImageImporter/RandomImageImporter.scala
@@ -1,7 +1,7 @@
 package ASCIIArt.ImageImporter.RandomImageImporter
 
 import ASCIIArt.Image.PixelGrid.Pixel.RGBPixel
-import ASCIIArt.Image.PixelGrid.RGBPixelGrid
+import ASCIIArt.Image.PixelGrid.PixelGrid
 import ASCIIArt.Image.RGBImage
 import ASCIIArt.ImageImporter.ImageImporter
 
@@ -12,14 +12,14 @@ class RandomImageImporter extends ImageImporter{
     val rng = new Random()
     val height = rng.nextInt(400) + 200
     val width = rng.nextInt(400) + 400
-    val pixelGrid = new RGBPixelGrid()
+    var pixelGrid = new PixelGrid[RGBPixel]()
     for (y <- 0 until height) {
-      val pixelLine = Vector[RGBPixel]()
+      var pixelLine = Vector[RGBPixel]()
       for (x <- 0 until width) {
         val pixel = RGBPixel(rng.nextInt(255), rng.nextInt(255), rng.nextInt(255))
-        pixelLine.appended(pixel)
+        pixelLine = pixelLine.appended(pixel)
       }
-      pixelGrid.appended(pixelLine)
+      pixelGrid = pixelGrid.appended(pixelLine)
     }
     RGBImage(pixelGrid)
   }
