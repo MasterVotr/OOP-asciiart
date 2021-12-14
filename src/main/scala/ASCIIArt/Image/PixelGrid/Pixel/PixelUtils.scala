a01023253d3d8ffe3f2cffe793a44cfe81943264
Jakub Votrubec <votruja6@fit.cvut.cz>
2021-12-14 11:51:32 +0100

    Added an exception to throw when combining 0 pixels

--- src/main/scala/ASCIIArt/Image/PixelGrid/Pixel/PixelUtils.scala
+++ src/main/scala/ASCIIArt/Image/PixelGrid/Pixel/PixelUtils.scala
@@ -2,6 +2,8 @@ package ASCIIArt.Image.PixelGrid.Pixel
 
 object PixelUtils {
   def CombineGreyscalePixels(pixels: List[GreyscalePixel]): GreyscalePixel = {
+    if (pixels.isEmpty)
+      throw new Exception("Cannot combine 0 pixels!")
     var sumBrightness = 0
     for (pixel <- pixels)
       sumBrightness += pixel.brightness
