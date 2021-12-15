51c7ba1f7ee55cd57e5ccac2656b3d2a98c1afee
Jakub Votrubec <votruja6@fit.cvut.cz>
2021-12-15 17:07:11 +0100

    Added MainTest system test

---
+++ src/test/scala/ASCIIArt/MainTest.scala
@@ -0,0 +1,55 @@
+package ASCIIArt
+
+import ASCIIArt.Controller.ConsoleController.ConsoleController
+import ASCIIArt.Image2ImageConvertor.RGB2GreyscaleConvertor
+import org.scalatest.FunSuite
+
+import scala.io.Source
+
+class MainTest extends FunSuite {
+  test("Life, Universe and Everything") {
+    assert(42 == 42)
+  }
+
+  test("Standard application run") {
+    val out_path = "output/gradient.txt"
+    val testArgs: List[String] = List(
+      "--image",
+      "resources/test_gradient.png",
+      "--scale",
+      "4",
+      "--rotate",
+      "180",
+      "--brightness",
+      "20",
+      "--invert",
+      "--flip",
+      "x",
+      "--scale",
+      "0.25",
+      "--output-file",
+      out_path,
+    )
+    // Parse test args
+    val (i, fs, es) = ConsoleController.parseArgs(testArgs)
+    // Import image
+    val rgbImg = i.ImportImage()
+    // Convert to greyscale
+    var greyImg = new RGB2GreyscaleConvertor().Convert(rgbImg)
+    // Apply filters
+    for (f <- fs)
+      greyImg = f.Execute(greyImg)
+    // Export
+    for (e <- es)
+      greyImg = e.Execute(greyImg)
+
+    // Load result
+    val bufferedFile = Source.fromFile(out_path)
+    var content = ""
+    for (line <- bufferedFile.getLines())
+      content += line
+
+    // Test result
+    assert(content == "@@@%#*+=-:")
+  }
+}
